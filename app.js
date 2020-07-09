33e0c6f3224da83eff9fdc8f0a0c3ffddfc714d6
Christopher Holmes <holmes.chris94@yahoo.com>
2020-07-09 16:32:20 -0400

    Added basic questions

--- app.js
+++ app.js
@@ -10,8 +10,41 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Create array to push employees into
+const employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
+function createEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is this employee's name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is this employee's email?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is this employee's id number?"
+        },
+        {
+            type: "list",
+            name: "job",
+            message: "What is this employee's job?",
+            choices: [
+                "Manager",
+                "Intern",
+                "Engineer"
+            ]
+        }
+    ])
+}
+
+createEmployee();
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
